18c40c5684f5c8d87af0fe956a6f7a2da08aa612
Yu Jin Choe <forworkchoe@gmail.com>
2025-03-26 22:46:28 +0900

    Add comments

--- src/app/admin-notice/[id]/edit/page.tsx
+++ src/app/admin-notice/[id]/edit/page.tsx
@@ -9,6 +9,15 @@ export default function Page() {
   const onSubmit = useCallback((event: FormEvent<HTMLFormElement>) => {
     // validate form data
 
+    /** Scenario
+     * 1. Enter the admin notice list page.
+     * 2. Fetch the notice list with cache 'no-store'.
+     * 3. Enter a specific notice edit page.
+     * 4. (Edit the notice)
+     * 5. Submit the form.
+     * 6. Return to the list page.
+     * 7. But the notice list page is still not updated because it remains in the client router cache.
+     */
     refresh();
     event.preventDefault();
     push('/admin-notice/list');
